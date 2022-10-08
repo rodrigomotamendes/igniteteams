c0e7344d71910beea9d74efd8cfd272ff3664cd0
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-10-08 17:58:27 -0300

    Criando Componente GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 import { Container } from './styles';
 
@@ -12,6 +13,8 @@ export function Groups() {
         title='Turmas'
         subtitle='jogue com a sua turma'
       />
+
+      <GroupCard title='Galera do Ignite'/>
     </Container>
   );
 }
