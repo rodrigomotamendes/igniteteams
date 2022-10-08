3401151617b5705fd7ad3201c13088b9642494a8
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-10-08 15:29:43 -0300

    Estilizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -12,6 +13,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor='transparent'
+        translucent
+      />
       { fontsLoaded ? <Groups/> : <Loading/> }
     </ThemeProvider>
   );
