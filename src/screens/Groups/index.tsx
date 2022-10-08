f6e342b204c63551ef563244643d25feda413a29
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-10-08 16:10:22 -0300

    Criando Componente Highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 
 import { Container } from './styles';
 
@@ -6,6 +7,11 @@ export function Groups() {
   return (
     <Container>
       <Header/>
+      
+      <Highlight
+        title='Turmas'
+        subtitle='jogue com a sua turma'
+      />
     </Container>
   );
 }
