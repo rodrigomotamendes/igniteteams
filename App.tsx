7c0fd5f57a3f0ab110fceb957389fd76cd958be0
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-10-08 15:26:03 -0300

    Componente de Loading

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
 
 import theme from './src/theme';
 
@@ -9,11 +10,9 @@ import { Groups } from '@screens/Groups';
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-
-
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups/> : <ActivityIndicator/> }
+      { fontsLoaded ? <Groups/> : <Loading/> }
     </ThemeProvider>
   );
 }
