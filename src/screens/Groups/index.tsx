ff9d208ed9c769476d65000ed63bb63b80d85d78
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-10-10 09:32:29 -0300

    Componente List Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,11 +4,12 @@ import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 import { Container } from './styles';
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -25,6 +26,12 @@ export function Groups() {
         renderItem={({ item }) => (
           <GroupCard 
             title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message='Que tal cadastrar a primeira turma?'
           />
         )}
       />
