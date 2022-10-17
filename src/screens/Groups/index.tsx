2fea1ba72d69aa17de4ec9aa60afebfd9cc22741
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-10-17 08:21:11 -0300

    Recap

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -35,6 +35,7 @@ export function Groups() {
             message='Que tal cadastrar a primeira turma?'
           />
         )}
+        showsVerticalScrollIndicator={false}
       />
 
       <Button 
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -10,7 +10,7 @@ import { ListEmpty } from '@components/ListEmpty';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { PlayerCard } from '@components/PlayerCard';
 
-import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 
 export function Players(){
   const [team, setTeam] = useState('Time A');
@@ -49,9 +49,9 @@ export function Players(){
           horizontal
         />
 
-        <NumbersOfPlayers>
+        <NumberOfPlayers>
           {players.length}
-        </NumbersOfPlayers>
+        </NumberOfPlayers>
       </HeaderList>
 
       <FlatList
