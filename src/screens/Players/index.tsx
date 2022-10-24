ce75b0fd621ace91b6cc7a395df55a2917ee0fe0
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-10-24 17:03:43 -0300

    Carregando Players pelo Grupo e Time

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -2,6 +2,12 @@ import { useState } from 'react';
 import { Alert, FlatList } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 
+import { AppError } from '@utils/AppError';
+
+import { PlayerStorageDTO } from '@storage/player/PlayerStorageDTO';
+import { playerAddByGroup } from '@storage/player/playerAddByGroup';
+import { playersGetByGroupAndTeam } from '@storage/player/playersGetByGroupAndTeam';
+
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import { Filter } from '@components/Filter';
@@ -10,10 +16,6 @@ import { Highlight } from '@components/Highlight';
 import { ListEmpty } from '@components/ListEmpty';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { PlayerCard } from '@components/PlayerCard';
-import { AppError } from '@utils/AppError';
-
-import { playerAddByGroup } from '@storage/players/playerAddByGroup';
-import { playersGetByGroup } from '@storage/players/PlayersGetByGroup';
 
 import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 
@@ -24,7 +26,7 @@ type RouteParams = {
 export function Players() {
   const [newPlayerName, setNewPlayerName] = useState('');
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState<PlayerStorageDTO[]>([]);
 
   const route = useRoute();
   const { group } = route.params as RouteParams;
@@ -44,8 +46,6 @@ export function Players() {
 
     try {
       await playerAddByGroup(newPlayer, group);
-      const players = await playersGetByGroup(group);
-      console.log(players);
     } catch (error) {
       if (error instanceof AppError) {
         Alert.alert('Nova pessoa', error.message);
@@ -53,6 +53,20 @@ export function Players() {
         console.log(error);
         Alert.alert('Nova pessoa', 'Não foi possível adicionar');
       }
+    }
+  }
+
+  async function fetchPlayersByTeam() {
+    try {
+      const playersByTeam = await playersGetByGroupAndTeam(group, team);
+
+      setPlayers(playersByTeam);
+    } catch (error) {
+      console.log(error);
+      Alert.alert(
+        'Pessoas',
+        'Não foi possível carregar as pessoas do time selecionado'
+      );
     }
   }
 
--- src/storage/player/playerAddByGroup.ts
+++ src/storage/player/playerAddByGroup.ts
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AppError } from '@utils/AppError';
 
 import { PLAYER_COLLECTION } from '@storage/storageConfig';
-import { playersGetByGroup } from './PlayersGetByGroup';
+import { playersGetByGroup } from './playersGetByGroup';
 import { PlayerStorageDTO } from './PlayerStorageDTO';
 
 export async function playerAddByGroup(
